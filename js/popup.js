4f9897c5705f8af8c98516c808ba87914ebea91c
Charlezzz <91009541+Automata01@users.noreply.github.com>
2025-04-02 08:25:24 +0800

    Delete js directory

--- js/popup.js
+++
@@ -1,7 +0,0 @@
-function showMessage() {
-    document.getElementById("aboutme-message").style.display = "block";
-}
-
-function ExitMessage() {
-    document.getElementById("aboutme-message").style.display = "none";
-}
